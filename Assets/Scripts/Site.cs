1672b0a7422fa3900e78bed568a710557a1ba888
Markus Wagner <prttmprpht@gmail.com>
2023-09-24 21:59:25 +0200

    Increase stability of beamforming

--- Assets/Scripts/Site.cs
+++ Assets/Scripts/Site.cs
@@ -1,20 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vuforia;
 
 public class Site : MonoBehaviour
 {
     public int networkVersion = 5;
     private Range range;
 
+    private Vuforia.ImageTargetBehaviour imageTarget;
+    private bool tracked = false;
+
     void Start()
     {
         range = GetComponent<Range>();
+        imageTarget = gameObject.GetComponentInParent<Vuforia.ImageTargetBehaviour>();
+
 
         if (this.networkVersion == 4)
         {
-            this.GetComponent<MeshRenderer>().material.color = new Color(0.5566038F, 0.5566038F, 0, 1);
+            // this.GetComponent<MeshRenderer>().material.color = new Color(0.5566038F, 0.5566038F, 0, 1);
         }
+
+        if (imageTarget)
+        {
+            imageTarget.OnTargetStatusChanged += OnTargetStatusChanged;
+        }
+    }
+
+    void OnTargetStatusChanged(ObserverBehaviour observerbehavour, TargetStatus status)
+    {
+        if ((status.Status == Status.TRACKED) && status.StatusInfo == StatusInfo.NORMAL)
+        {
+            tracked = true;
+        } else
+        {
+            tracked = false;
+        }
+    }
+
+    public bool IsTracked()
+    {
+        return tracked;
     }
 
     public Range GetRange()
--- Assets/Scripts/User.cs
+++ Assets/Scripts/User.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vuforia;
 
 public class User : MonoBehaviour
 {
@@ -12,25 +13,58 @@ public class User : MonoBehaviour
     private GameObject canvas;
     private textboxController textboxController;
 
+    private Vuforia.ImageTargetBehaviour imageTarget;
+    private bool tracked = false;
+
     void Start()
     {
         potentialTargets = GameObject.FindGameObjectsWithTag("Site");
        //  range = GetComponent<Range>();
         renderer = GetComponent<LineRenderer>();
+        renderer.enabled = false;
 
         //TextBox Controller variables definition
         canvas = GameObject.Find("Canvas");
         textboxController = (textboxController) canvas.GetComponent(typeof(textboxController));
+
+        imageTarget = gameObject.GetComponentInParent<Vuforia.ImageTargetBehaviour>();
+
+        if (imageTarget)
+        {
+            imageTarget.OnTargetStatusChanged += OnTargetStatusChanged;
+        }
+    }
+
+    void OnTargetStatusChanged(ObserverBehaviour observerbehavour, TargetStatus status)
+    {
+        if ((status.Status == Status.TRACKED) && status.StatusInfo == StatusInfo.NORMAL)
+        {
+            tracked = true;
+        }
+        else
+        {
+            this.renderer.enabled = false;
+            tracked = false;
+        }
     }
 
     void Update()
     {
+        if (!tracked)
+        {
+            this.renderer.enabled = false;
+            return;
+        }
+
         Site eligibleTarget = null;
         double lastDist = double.MaxValue;
 
         foreach (GameObject potentialTarget in potentialTargets)
         {
             Site site = potentialTarget.GetComponent<Site>();
+
+            if (!site.IsTracked()) continue;
+
             //float usrMaxRange = range.range;
             float siteMaxRange = site.GetRange().range;
             float requiredRange = siteMaxRange; //Mathf.Min(usrMaxRange, siteMaxRange);
