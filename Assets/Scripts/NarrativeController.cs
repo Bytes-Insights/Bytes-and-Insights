c50b9f9b03403bd8722a4c6c2c292ddf459bc495
Markus Wagner <prttmprpht@gmail.com>
2023-09-29 00:30:51 +0200

    Fix scroll of explanation, add explanation text for AIR6488

--- Assets/Scripts/NarrativeController.cs
+++ Assets/Scripts/NarrativeController.cs
@@ -26,9 +26,8 @@ public class NarrativeController : MonoBehaviour
 
     public void ExplainSites()
     {
-        ExplainAIR6488();
-        //sitesExplained = true;
-        //explanation.ShowExplanation("Sites", "Places where technological equipment, such as antennas and radios, are located, are called 'sites'.");
+        sitesExplained = true;
+        explanation.ShowExplanation("Sites", "Places where technological equipment, such as antennas and radios, are located, are called 'sites'.");
     }
 
     public bool WereSitesExplained()
@@ -62,7 +61,9 @@ public class NarrativeController : MonoBehaviour
     public void ExplainAIR6488()
     {
         AIR6488Explained = true;
-        explanation.ShowExplanation("AIR6488", "4G is a fast wireless technology that allows us to use our phones and devices for internet, streaming, gaming, and downloading. It's not as fast as 5G, but it's still much faster than older networks like 3G.", 1);
+        explanation.ShowExplanation("AIR6488", "This is an AIR unit, that is, an 'Antenna-integrated radio'. By integrating the radio unit with the antenna in a single device, this equipment allows service providers to deploy 5G networks without the need for extra physical space.\n" +
+"It utilizes technologies such as 'Massive MIMO' (Multiple Input, Multiple Output) and 'beamforming', allowing for improved coverage, capacity and user experience (by supporting higher data rates and lower latencies).\n" +
+"It has a compact design with average dimensions of about 80cm x 50cm x 25cm and 45 kg. The unit can work under a wide range of environmental operation conditions (such as temperatures from -40 to +55C and relative humidity from 2% to 100%).", 1);
     }
     public bool WasAIR6488Explained()
     {
