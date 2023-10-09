f226acf532d4f5053a717e56d87d4cefc220884a
malinjam <malin.marques@gmail.com>
2023-10-09 13:17:56 +0200

    building opacity down on occlusion: ready for testing

--- Assets/Scripts/OnCollision.cs
+++ Assets/Scripts/OnCollision.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class OnCollision : MonoBehaviour
 {
-    public Color colour;
+	public GameObject building;
+	private Color colour;
 	private double opacity;
 	private double factor;
 	private bool colliding;
 	
 	void Start()
 	{
-		GameObject building = FindChildWithTag("Cube");
 		colour=building.GetComponent<MeshRenderer>().material.color;
+		System.Console.WriteLine("building identified");
 		opacity=colour.a;
 	}
 	
@@ -22,11 +23,12 @@ public class OnCollision : MonoBehaviour
 		{
 				factor-=0.1;
 				opacity=opacity*factor;
+				System.Console.WriteLine("decrease opacity");
         }
 		else if(colliding==false & factor!=1){
 				factor+=0.1;
 				opacity=opacity*factor;
-			
+			System.Console.WriteLine("increase opacity");
 		}
 		
     }
@@ -34,10 +36,12 @@ public class OnCollision : MonoBehaviour
 	private void OnTriggerEnter(Collider other)
 	    {
 	        colliding=true;
+			System.Console.WriteLine("collided");
 	    }
 		
 	private void OnTriggerExit(Collider other)
 		    {
 		        colliding=false;
+				System.Console.WriteLine("separated");
 		    }
 }
