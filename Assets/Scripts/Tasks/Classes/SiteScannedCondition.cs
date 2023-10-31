9f27f01cf16c6cccd50605a84fbbfa6f19b1af09
Markus Wagner <prttmprpht@gmail.com>
2023-10-31 22:44:29 +0100

    Add site scanned condition

---
+++ Assets/Scripts/Tasks/Classes/SiteScannedCondition.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SiteScannedCondition : MonoBehaviour
+{
+    public Subtask subtask;
+
+    void Update()
+    {
+        if (subtask.getCompleted())
+        {
+            return;
+        }
+
+        GameObject[] sites = GameObject.FindGameObjectsWithTag("Site_Controller");
+
+        foreach (GameObject obj in sites)
+        {
+            Site controller = obj.GetComponent<Site>();
+
+            if (controller && controller.IsTracked())
+            {
+                subtask.setCompleted(true);
+                return;
+            }
+        }
+    }
+}
