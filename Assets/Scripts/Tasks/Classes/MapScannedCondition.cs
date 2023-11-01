a4c8b5d30cc3a3b00a4ee81a37d91b3f4face957
Ferran Campos <ferran98campos@gmail.com>
2023-11-01 16:48:45 +0100

    Database update

---
+++ Assets/Scripts/Tasks/Classes/MapScannedCondition.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapScannedCondition : SubtaskCondition
+{
+    public MapScannedCondition(Subtask s){
+        subtask = s;
+        canBeCompleted = false;
+    }
+
+    override public bool checkCondition()
+    {
+        if (!canBeCompleted && subtask.getCompleted())
+        {
+            return false;
+        }
+
+        GameObject[] sites = GameObject.FindGameObjectsWithTag("Site_Controller");
+
+        foreach (GameObject obj in sites)
+        {
+            Site controller = obj.GetComponent<Site>();
+
+            if (controller && controller.IsTracked())
+            {
+                isCompleted = true;
+                break;
+            }
+        }
+
+        return isCompleted;
+    }
+
+}
