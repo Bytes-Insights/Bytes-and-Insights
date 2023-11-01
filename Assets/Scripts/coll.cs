81cf8f0e6f94a4accb0bb202a29aa32b4fea6aba
maria <maria.zelenika@gmail.com>
2023-11-02 00:54:36 +0100

    wrote new script for collision

---
+++ Assets/Scripts/coll.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coll : MonoBehaviour
+{
+    public GameObject building;
+    private Material currentMat;
+    private bool colliding;
+
+    void Start()
+    {
+        colliding = false;
+        currentMat = gameObject.GetComponent<Renderer>().material;
+    }
+
+    void Update()
+    {
+        if (colliding)
+        {
+            // Decrease opacity when colliding
+            float currentOpacity = currentMat.GetFloat("_Opacity");
+            currentMat.SetFloat("_Opacity", currentOpacity - 1.0f);
+        }
+        else
+        {
+            // Increase opacity when not colliding
+            float currentOpacity = currentMat.GetFloat("_Opacity");
+            currentMat.SetFloat("_Opacity", currentOpacity + 1.0f);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        colliding = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        colliding = false;
+    }
+}
